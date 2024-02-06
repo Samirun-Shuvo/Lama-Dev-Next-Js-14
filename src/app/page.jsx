c451b43e6274ec7315c6b43701a6004b6f64b9df
Samirun-Shuvo <samirunshuvo@gmail.com>
2024-02-06 23:21:18 +0600

    home section update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita,
+          dignissimos iste. Quas praesentium dignissimos laborum laudantium illo
+          doloremque velit vitae?
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image className={styles.brandImg} src="/brands.png" alt="" fill />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image className={styles.heroImg} src="/hero.gif" alt="" fill />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
