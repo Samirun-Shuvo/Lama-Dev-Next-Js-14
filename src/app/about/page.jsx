dfbaf0050426deddc8a65c371fb4249be9599a89
Md-Samirun-Shuvo <shuvo@excelitai.com>
2024-02-06 17:10:25 +0600

    home page update

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import Image from "next/image";
+import React from "react";
+import styles from "./about.module.css";
 
 const AboutPage = () => {
-    return (
-        <div>
-            About Page
-        </div>
-    );
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image src="https://images.pexels.com/photos/259915/pexels-photo-259915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill></Image>
+      </div>
+    </div>
+  );
 };
 
-export default AboutPage
+export default AboutPage;
