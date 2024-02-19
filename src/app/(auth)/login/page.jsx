bc4cf7ac982b2b51a1c5a491cba8d74aaa04cb58
Md-Samirun-Shuvo <shuvo@excelitai.com>
2024-02-19 15:53:20 +0600

    auth update

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import { auth, signIn } from "@/lib/auth";
+import React from "react";
 
-const LoginPage = () => {
-    return (
-        <div>
-            Login Page
-        </div>
-    );
+const LoginPage = async () => {
+  const session = await auth();
+  console.log(session);
+  const handleGithubLogin = async () => {
+    "use server";
+    await signIn("github");
+  };
+  return (
+    <div>
+      <form action={handleGithubLogin}>
+        <button>Login with Github</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
